1ee6fe1577d9d4d86d23970e860fb52c42a89875
Geibinger <friedl.jak@gmail.com>
2023-06-16 13:59:39 +0200

    added config for SMTP server

--- config-example.py
+++ config-example.py
@@ -1,3 +1,5 @@
 # config.py
 username = 'YOUR_USERNAME'
 password = 'YOUR_PASSWORD'
+smtp_server = 'smtp.easyname.com'
+smtp_port = 465
--- notifier.py
+++ notifier.py
@@ -2,10 +2,15 @@ import pandas as pd
 import smtplib
 import ssl
 from datetime import datetime
-from config import username, password  # Import sensitive information
+from openpyxl import load_workbook
+from config import username, password, smtp_server, smtp_port  # Import sensitive information
+
+# Load the Excel file
+wb = load_workbook(filename='Your_File.xlsx')
+sheet = wb.active
 
 # Read the Excel file into a pandas DataFrame
-df = pd.read_excel('testfile.xlsx')
+df = pd.read_excel('Your_File.xlsx')
 
 # Get today's date
 today = datetime.today().strftime('%Y-%m-%d')
@@ -18,7 +23,7 @@ for index, row in df.iterrows():
         context = ssl.create_default_context()
 
         # Use the SMTP client
-        with smtplib.SMTP_SSL("smtp.easyname.com", 465, context=context) as server:
+        with smtplib.SMTP_SSL(smtp_server, smtp_port, context=context) as server:
             server.login(username, password)
 
             # Send email
